be4c8b25796aae2f5bb336bb98a05e2a5d4af683
Karan <22f2001061@ds.study.iitm.ac.in>
2024-07-16 12:49:30 +0530

    Created register route and  added User Model Definition

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-from flask import Flask, request, render_template, url_for
+from flask import Flask, request, render_template, url_for, redirect, flash
+from models import User
+from config import LocalConfig
+from db import db
+from werkzeug.security import generate_password_hash
 
 app = Flask(__name__)
+app.config.from_object(LocalConfig)
+db.init_app(app)
 
 
 # http://localhost:5000/
@@ -9,10 +15,49 @@ def homepage():
     return render_template("homepage.html")
 
 
-@app.route("/register")
+@app.route("/register", methods=["GET", "POST"])
 def register():
-    return render_template("register.html")
+    if request.method == "GET":
+        return render_template("register.html")
+    elif request.method == "POST":
+        # get the form data from request
+        first_name = request.form.get("fname")
+        lname = request.form.get("lname")
+        username = request.form.get("username")
+        email = request.form.get("email")
+        password = request.form.get("password")
+        confirmPassword = request.form.get("cofirmPassword")
+        if password == confirmPassword:
+            pass_hash = generate_password_hash(password)
+        else:
+            flash("Confirm password should be same as Passord", "warning")
+            return redirect(url_for("register"))
+        # make an entry into users table to create a new user
+        if pass_hash:
+            new_user = User(
+                fname=first_name,
+                lname=lname,
+                username=username,
+                email=email,
+                role="student",
+                password=pass_hash,
+            )
+            db.session.add(new_user)
+            db.session.commit()
+
+        # return the response or redirect to login
+        return redirect(url_for("homepage"))
+
+
+@app.route("/create-tables")
+def create_tables():
+    try:
+        with app.app_context():
+            db.create_all()
+    except Exception as e:
+        return f"Something Went wrong {str(e)}"
+    return "TABLES CREATED SUCCESFULLY"
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
---
+++ models.py
@@ -0,0 +1,87 @@
+from db import db
+
+
+class User(db.Model):
+    __tablename__ = "users"
+    user_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    fname = db.Column(db.String, nullable=False)
+    lname = db.Column(db.String)
+    username = db.Column(db.String, unique=True, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, unique=True, nullable=False)
+    role = db.Column(db.String)
+
+    # class Book(db.Model):
+    #     __tablename__ = "books"
+    #     book_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    #     title = db.Column(db.String, nullable=False)
+    #     description = db.Column(db.String, nullable=True)
+    #     content = db.Column(db.String, nullable=False)
+    #     author = db.Column(db.String, nullable=False)
+    #     # pages
+    #     # thumbnail
+    #     section_id = db.Column(
+    #         db.Integer, db.ForeignKey("sections.section_id"), nullable=True
+    #     )
+
+    # class Section(db.Model):
+    #     __tablename__ = "sections"
+    #     section_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    #     section_name = db.Column(db.String)
+    #     books = db.relationship("Book", backref="books", lazy=True)
+
+    # class BookIssue(db.Model):
+    #     __name__ = "book_issues"
+    #     id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    #     book_id = db.Column(db.Integer, db.ForeignKey("books.book_id"), nullable=False)
+    #     user_id = db.Column(db.Integer, db.ForeignKey("users.user_id"))
+    #     date_issued = db.Column(db.Date, nullable=True)
+    #     date_returned = db.Column(db.Date, nullable=True)
+    #     is_revoked = db.Column(db.Boolean, default=False)
+    #     request_accepted = db.Column(db.Boolean, default=False)
+
+    # class BaseUser(db.Model):
+    #     # id
+    #     # username
+    #     # password
+    #     # email
+    #     # role
+    #     pass
+
+    # class InfluencerDetails(db.Model):
+    # id
+    #   user_id = foreignkey(user.user_id)
+    #   url
+    # platform_name
+    # number of following
+    ...
+
+    #     # yturl
+    #     # insta
+    #     # ...
+    #     pass
+
+    # class SponsorDetails(db.Model):
+    #     # id
+    #     # username
+    #  industry name
+    #     # yturl
+    #     # insta
+    #     # ...
+    #     pass
+
+    # class Campaign():
+    id
+    # name
+    # description
+    # useri_id
+    # status
+    # budget
+
+
+# class CampaignRequest():
+# user_id
+# campaign_id
+# is_approved
+# request_from
+# request_to
